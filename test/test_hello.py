be9e0e36f02d5a77e034dc3e6e01c30cd64a006f
Michael Brown <mbrown@fensystems.co.uk>
2019-08-22 19:33:28 +0100

    test: Add tests for the Hello World API
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

---
+++ test/test_hello.py
@@ -0,0 +1,51 @@
+"""Hello World API tests"""
+
+from hmrc.api import HmrcClientError
+from hmrc.api.hello import HelloClient
+from . import TestCase, anonymous, application, individual, organisation
+
+
+class HelloTest(TestCase):
+    """Hello World API tests"""
+
+    Client = HelloClient
+
+    @anonymous
+    def test_world(self, client):
+        """Test open access endpoint"""
+        msg = client.world()
+        self.assertEqual(msg.message, "Hello World")
+        with self.assertRaises(HmrcClientError):
+            client.application()
+        with self.assertRaises(HmrcClientError):
+            client.user()
+
+    @application
+    def test_application(self, client):
+        """Test application-restricted endpoint"""
+        msg = client.world()
+        self.assertEqual(msg.message, "Hello World")
+        msg = client.application()
+        self.assertEqual(msg.message, "Hello Application")
+        with self.assertRaises(HmrcClientError):
+            client.user()
+
+    @individual()
+    def test_individual(self, client):
+        """Test user-restricted endpoint with individual test user"""
+        msg = client.world()
+        self.assertEqual(msg.message, "Hello World")
+        msg = client.application()
+        self.assertEqual(msg.message, "Hello Application")
+        msg = client.user()
+        self.assertEqual(msg.message, "Hello User")
+
+    @organisation()
+    def test_organisation(self, client):
+        """Test user-restricted endpoint with organisation test user"""
+        msg = client.world()
+        self.assertEqual(msg.message, "Hello World")
+        msg = client.application()
+        self.assertEqual(msg.message, "Hello Application")
+        msg = client.user()
+        self.assertEqual(msg.message, "Hello User")
