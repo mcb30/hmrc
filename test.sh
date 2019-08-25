aac21e4b2b9e44dcf2543beb3144c3235a6af208
Michael Brown <mbrown@fensystems.co.uk>
2019-08-25 16:54:10 +0100

    test: Run pylint as part of the Travis CI tests
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- test.sh
+++ test.sh
@@ -3,6 +3,25 @@
 set -e
 set -x
 
+# Run test suite with coverage checks
+#
 coverage3 erase
 coverage3 run --branch --source hmrc setup.py test
 coverage3 report --show-missing
+
+# Run pylint
+#
+# Note that pylint is currently broken on Python 3.8 beta, so run only
+# if sanity check passes.  This check should be removable once Python
+# 3.8 is released.
+#
+cat > .pylint-check.py <<EOF
+"""Pylint sanity check"""
+from typing import Type
+check = Type
+EOF
+if pylint .pylint-check.py >/dev/null 2>/dev/null ; then
+    pylint hmrc
+else
+    echo Skipping pylint check
+fi
