6c83501aed7399e15f5d5b7f7adbadedc2698109
Michael Brown <mbrown@fensystems.co.uk>
2019-08-30 23:37:17 +0100

    test: Add tests for "Hello World" command line interface
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- test/test_hello.py
+++ test/test_hello.py
@@ -49,3 +49,27 @@ class HelloTest(TestCase):
         self.assertEqual(msg.message, "Hello Application")
         msg = client.user()
         self.assertEqual(msg.message, "Hello User")
+
+
+class HelloCommandTest(TestCase):
+    """Hello World command line tests"""
+
+    Client = HelloClient
+
+    @anonymous
+    def test_world(self, client):
+        """Test open access endpoint"""
+        output = self.command(client, "hello world")
+        self.assertEqual(output, "Hello World")
+
+    @application
+    def test_application(self, client):
+        """Test application-restricted endpoint"""
+        output = self.command(client, "hello application")
+        self.assertEqual(output, "Hello Application")
+
+    @individual()
+    def test_user(self, client, _user):
+        """Test user-restricted endpoint"""
+        output = self.command(client, "hello user")
+        self.assertEqual(output, "Hello User")
