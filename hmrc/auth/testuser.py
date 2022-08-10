24ef1df1651e110e43a2fa67f6bb37222892205b
Michael Brown <mbrown@fensystems.co.uk>
2022-08-10 23:43:11 +0100

    test: Update to current HMRC test user authentication flow
    Signed-off-by: Michael Brown <mbrown@fensystems.co.uk>

--- hmrc/auth/testuser.py
+++ hmrc/auth/testuser.py
@@ -65,7 +65,11 @@ class TestUserAuthClient(WebApplicationClient):
             uri, page = self.fetch_auth_page(session, uri)
 
             # Click on the initial "Continue" button
-            href = page.find('.//a[button]').get('href')
+            href = page.find('.//a[@role="button"]').get('href')
+            uri, page = self.fetch_auth_page(session, urljoin(uri, href))
+
+            # Click on the "Sign in to Government Gateway" button
+            href = page.find('.//a[@role="button"]').get('href')
             uri, page = self.fetch_auth_page(session, urljoin(uri, href))
 
             # Submit sign in form
